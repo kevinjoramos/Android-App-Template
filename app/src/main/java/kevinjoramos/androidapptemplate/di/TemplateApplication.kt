c7d4e889b54e02bb489a15721b55bba2e0636618
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-08-17 07:51:17 -0600

    Created package structure.

--- app/src/main/java/kevinjoramos/androidapptemplate/di/TemplateApplication.kt
+++ app/src/main/java/kevinjoramos/androidapptemplate/di/TemplateApplication.kt
@@ -1,4 +1,4 @@
-package kevinjoramos.androidapptemplate
+package kevinjoramos.androidapptemplate.di
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
--- app/src/main/java/kevinjoramos/androidapptemplate/ui/MainActivity.kt
+++ app/src/main/java/kevinjoramos/androidapptemplate/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package kevinjoramos.androidapptemplate
+package kevinjoramos.androidapptemplate.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
