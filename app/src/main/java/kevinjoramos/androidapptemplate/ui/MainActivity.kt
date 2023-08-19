60922a21587194cd8060e39c328eb398037528a0
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-08-18 20:39:27 -0600

    Completed adding basic splash screen.
    Using the new splash screen api.

--- app/src/main/java/kevinjoramos/androidapptemplate/ui/MainActivity.kt
+++ app/src/main/java/kevinjoramos/androidapptemplate/ui/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kevinjoramos.androidapptemplate.ui.navigation.TemplateNavigation
 import kevinjoramos.androidapptemplate.ui.theme.AndroidAppTemplateTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             AndroidAppTemplateTheme {
                 // A surface container using the 'background' color from the theme
