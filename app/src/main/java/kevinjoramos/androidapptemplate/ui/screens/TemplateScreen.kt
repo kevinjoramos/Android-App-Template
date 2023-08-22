da0905b018a1a33dae2896b305794cace930c29c
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-08-22 04:42:48 -0600

    Added application class to android manifest.

--- app/src/main/java/kevinjoramos/androidapptemplate/ui/screens/TemplateScreen.kt
+++ app/src/main/java/kevinjoramos/androidapptemplate/ui/screens/TemplateScreen.kt
@@ -1,8 +1,9 @@
 package kevinjoramos.androidapptemplate.ui.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun TemplateScreen() {
-
+    Text(text = "Hello World! ~Kevin")
 }
