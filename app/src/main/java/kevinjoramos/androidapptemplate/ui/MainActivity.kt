f4edcc4575483ae564f8e15aafae01d21199e98d
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-08-30 23:19:50 -0600

    Removed unused imports from main activity

--- app/src/main/java/kevinjoramos/androidapptemplate/ui/MainActivity.kt
+++ app/src/main/java/kevinjoramos/androidapptemplate/ui/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kevinjoramos.androidapptemplate.ui.navigation.TemplateNavigation
@@ -32,7 +29,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting("Android")
                     TemplateNavigation()
                 }
             }
@@ -40,18 +36,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidAppTemplateTheme {
-        Greeting("Android")
-    }
-}
